7a557bb5907d7577dabb741d67914724a1da3892
Brad Wadsworth <bwadsworth@digitalmeasures.com>
2017-12-22 15:42:18 -0600

    adding instance listing

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
@@ -17,7 +19,7 @@ func (s *ec2Filters) String() string {
 func (s *ec2Filters) Set(value string) error {
 	strs := strings.Split(value, ":")
 	filter := new(ec2.Filter)
-	filter.SetName(strs[0])
+	filter.SetName("tag:" + strs[0])
 	vals := strings.Split(strs[1], ",")
 	filterVals := make([]*string, len(vals))
 	for i := range vals {
@@ -30,62 +32,105 @@ func (s *ec2Filters) Set(value string) error {
 
 var filters ec2Filters
 
-func createFilters(f ec2Filters) []*ec2.Filter {
-	fs := make([]*ec2.Filter, 0)
-	// for k, v := range f {
-	// 	flt := new(ec2.Filter)
-	// 	flt.SetName(k)
-	// 	flt.SetValues(v)
-	// }
-	// filter.SetName(name)
-	// valuePnt := make([]*string, len(values))
-	// for i := range values {
-	// 	valuePnt[i] = &values[i]
-	// }
-	// filter.SetValues(valuePnt)
-	return fs
-}
+type instances []*ec2.Instance
 
-func assembleFilters(filter ...*ec2.Filter) []*ec2.Filter {
-	filters := make([]*ec2.Filter, 0)
-	for _, i := range filter {
-		filters = append(filters, i)
-	}
-	return filters
-}
-
-func getInstanceIds(instances *ec2.DescribeInstancesOutput) []*string {
-	strPnt := make([]*string, 0)
+func NewInstances(instances *ec2.DescribeInstancesOutput) instances {
+	inst := make([]*ec2.Instance, 0)
 	for _, v := range instances.Reservations {
 		for _, v := range v.Instances {
-			strPnt = append(strPnt, v.InstanceId)
+			inst = append(inst, v)
 		}
 	}
-	return strPnt
+	return inst
+}
+
+func (s *instances) InstanceIds() []string {
+	ids := make([]string, len(*s))
+	for i, v := range *s {
+		ids[i] = v.GoString()
+	}
+	return ids
+}
+
+func tagOutput(tag []*ec2.Tag) string {
+	var str string
+	for _, v := range tag {
+		str += fmt.Sprintf("  %s: %s\n", *v.Key, *v.Value)
+	}
+	return str
+}
+
+func (s *instances) shortOutput() {
+	num := fmt.Sprintf("Found %d instances\n", len(*s))
+	fmt.Println(strings.Repeat("-", len(num)))
+	fmt.Print(num)
+	for _, v := range *s {
+		name := fmt.Sprintf("Instance ID: %s\n", *v.InstanceId)
+		tags := fmt.Sprintf("%s", tagOutput(v.Tags))
+		fmt.Println(strings.Repeat("-", len(name)))
+		fmt.Print(name)
+		fmt.Println("Tags:")
+		fmt.Printf("%s\n", tags)
+	}
+}
+
+type instanceStates []*ec2.InstanceStateChange
+
+func NewInstanceStates(states *ec2.StopInstancesOutput) instanceStates {
+	return states.StoppingInstances
+}
+
+func (s *instanceStates) InstanceIds() []string {
+	inst := make([]string, len(*s))
+	for i, v := range *s {
+		inst[i] = *v.InstanceId
+	}
+	return inst
 }
 
 func main() {
 	flag.Var(&filters, "tag", "Tags key:value. example MyKey:Value1,Value2")
 	flag.Parse()
-	fmt.Println(filters.String())
-	// sess := session.Must(session.NewSessionWithOptions(session.Options{
-	// 	SharedConfigState: session.SharedConfigEnable,
-	// }))
-	// svc := ec2.New(sess)
-	// envFilter := createFilter("tag:Environment", "alpha")
-	// filters := assembleFilters(envFilter)
-	// instanceInput := new(ec2.DescribeInstancesInput)
-	// instanceInput.Filters = filters
-	// instances, _ := svc.DescribeInstances(instanceInput)
-	// instancesIds := getInstanceIds(instances)
-	// fmt.Println(instancesIds)
+	action := flag.Arg(0)
+	if action == "" {
+		os.Exit(1)
+	}
+	sess := session.Must(session.NewSessionWithOptions(session.Options{SharedConfigState: session.SharedConfigEnable}))
+	svc := ec2.New(sess)
+	instanceInput := new(ec2.DescribeInstancesInput)
+	instanceInput.Filters = filters
+	query, err := svc.DescribeInstances(instanceInput)
+	if err != nil {
+		fmt.Println(err)
+	}
+	insts := NewInstances(query)
+	insts.shortOutput()
+	if action == "list" {
+		os.Exit(0)
+	}
+	fmt.Printf("Are you sure you would like to %s the above instances (y|n)\n", action)
+	var verify string
+	_, err = fmt.Scan(&verify)
+	if err != nil {
+		fmt.Println(err)
+	}
+	switch verify {
+	case "y":
+		fmt.Printf("Performing %s action on instances\n", action)
+		if action == "stop" {
+			// 	stopInstances := new(ec2.StopInstancesInput)
+			// 	stopInstances.SetInstanceIds(insts.InstanceIds())
+			// 	output, err := svc.StopInstances(stopInstances)
+			// 	if err != nil {
+			// 		fmt.Print(err)
+			// 	}
+		}
+	case "n":
+		fmt.Println("Exiting and taking no action")
+	default:
+		fmt.Println("Answer must be y or n")
+	}
 
-	// stopInstances := new(ec2.StopInstancesInput)
-	// stopInstances.SetInstanceIds(instances)
-	// output, err := svc.StopInstances(stopInstances)
-	// if err != nil {
-	// 	fmt.Print(err)
-	// }
 	// for _, s := range output.StoppingInstances {
 	// 	fmt.Printf("%s %s", *s.InstanceId, s.CurrentState.String())
 	// }
