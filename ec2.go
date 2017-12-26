bd2219a0b88ddd027bb54fa3eb163cebff006218
Brad Wadsworth <bwadsworth@digitalmeasures.com>
2017-12-26 16:23:42 -0600

    split into multiple source files

---
+++ ec2.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+type ec2Filters []*ec2.Filter
+
+func (s *ec2Filters) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *ec2Filters) Set(value string) error {
+	strs := strings.Split(value, ":")
+	filter := new(ec2.Filter)
+	filter.SetName("tag:" + strs[0])
+	vals := strings.Split(strs[1], ",")
+	filterVals := make([]*string, len(vals))
+	for i := range vals {
+		filterVals[i] = &vals[i]
+	}
+	filter.SetValues(filterVals)
+	*s = append(*s, filter)
+	return nil
+}
+
+var filters ec2Filters
+
+func instances(res []*ec2.Reservation) []*ec2.Instance {
+	inst := make([]*ec2.Instance, 0)
+	for _, v := range res {
+		for _, i := range v.Instances {
+			inst = append(inst, i)
+		}
+	}
+	return inst
+}
+
+func instanceIds(inst []*ec2.Instance) []*string {
+	ids := make([]*string, len(inst))
+	for i, v := range inst {
+		ids[i] = v.InstanceId
+	}
+	return ids
+}
+
+func instanceOutput(res []*ec2.Reservation) string {
+	var str string
+	inst := instances(res)
+	for _, v := range inst {
+		id := fmt.Sprintf("Instance ID: %s\n", *v.InstanceId)
+		str += fmt.Sprintln(strings.Repeat("-", len(id)))
+		str += fmt.Sprintf(id)
+		str += fmt.Sprintf("State: %s\n", *v.State.Name)
+		str += fmt.Sprintln("Tags")
+		for _, t := range v.Tags {
+			str += fmt.Sprintf("  %s: %s\n", *t.Key, *t.Value)
+		}
+	}
+	return str
+}
+
+func instanceStateOutput(states []*ec2.InstanceStateChange) string {
+	var str string
+	for _, v := range states {
+		id := fmt.Sprintf("Instance ID: %s\n", *v.InstanceId)
+		str += fmt.Sprintln(strings.Repeat("-", len(id)))
+		str += fmt.Sprintln(id)
+		str += fmt.Sprintf("  Last State: %s\n", *v.PreviousState.Name)
+		str += fmt.Sprintf("  Current State: %s\n", *v.CurrentState.Name)
+	}
+	return str
+}
+
+func newDescribeInstanceInput(filters []*ec2.Filter) *ec2.DescribeInstancesInput {
+	input := new(ec2.DescribeInstancesInput)
+	st := "instance-state-name"
+	stRun := "running"
+	stStop := "stopped"
+	filters = append(filters, &ec2.Filter{Name: &st, Values: []*string{&stRun, &stStop}})
+	input.Filters = filters
+	return input
+}
+
+func newStopInstanceInput(query *ec2.DescribeInstancesOutput) *ec2.StopInstancesInput {
+	input := new(ec2.StopInstancesInput)
+	input.SetInstanceIds(instanceIds(instances(query.Reservations)))
+	return input
+}
+
+func newStartInstanceInput(query *ec2.DescribeInstancesOutput) *ec2.StartInstancesInput {
+	input := new(ec2.StartInstancesInput)
+	input.SetInstanceIds(instanceIds(instances(query.Reservations)))
+	return input
+}
--- main.go
+++ main.go
@@ -4,79 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
-
-type ec2Filters []*ec2.Filter
-
-func (s *ec2Filters) String() string {
-	return fmt.Sprint(*s)
-}
-
-func (s *ec2Filters) Set(value string) error {
-	strs := strings.Split(value, ":")
-	filter := new(ec2.Filter)
-	filter.SetName("tag:" + strs[0])
-	vals := strings.Split(strs[1], ",")
-	filterVals := make([]*string, len(vals))
-	for i := range vals {
-		filterVals[i] = &vals[i]
-	}
-	filter.SetValues(filterVals)
-	*s = append(*s, filter)
-	return nil
-}
-
-var filters ec2Filters
-
-func Instances(res []*ec2.Reservation) []*ec2.Instance {
-	inst := make([]*ec2.Instance, 0)
-	for _, v := range res {
-		for _, i := range v.Instances {
-			inst = append(inst, i)
-		}
-	}
-	return inst
-}
-
-func InstanceIds(inst []*ec2.Instance) []*string {
-	ids := make([]*string, len(inst))
-	for i, v := range inst {
-		ids[i] = v.InstanceId
-	}
-	return ids
-}
-
-func InstanceOutput(res []*ec2.Reservation) string {
-	var str string
-	inst := Instances(res)
-	for _, v := range inst {
-		id := fmt.Sprintf("Instance ID: %s\n", *v.InstanceId)
-		str += fmt.Sprintln(strings.Repeat("-", len(id)))
-		str += fmt.Sprintf(id)
-		str += fmt.Sprintf("State: %s\n", *v.State.Name)
-		str += fmt.Sprintln("Tags")
-		for _, t := range v.Tags {
-			str += fmt.Sprintf("  %s: %s\n", *t.Key, *t.Value)
-		}
-	}
-	return str
-}
-
-func InstanceStateOutput(states []*ec2.InstanceStateChange) string {
-	var str string
-	for _, v := range states {
-		id := fmt.Sprintf("Instance ID: %s\n", *v.InstanceId)
-		str += fmt.Sprintln(strings.Repeat("-", len(id)))
-		str += fmt.Sprintln(id)
-		str += fmt.Sprintf("  Last State: %s\n", *v.PreviousState.Name)
-		str += fmt.Sprintf("  Current State: %s\n", *v.CurrentState.Name)
-	}
-	return str
-}
 
 func main() {
 	flag.Var(&filters, "tag", "Tags key:value. example MyKey:Value1,Value2")
@@ -87,21 +18,15 @@ func main() {
 	}
 	sess := session.Must(session.NewSessionWithOptions(session.Options{SharedConfigState: session.SharedConfigEnable}))
 	svc := ec2.New(sess)
-	instanceInput := new(ec2.DescribeInstancesInput)
-	st := "instance-state-name"
-	stRun := "running"
-	stStop := "stopped"
-	filters = append(filters, &ec2.Filter{Name: &st, Values: []*string{&stRun, &stStop}})
-	instanceInput.Filters = filters
-	query, err := svc.DescribeInstances(instanceInput)
+	query, err := svc.DescribeInstances(newDescribeInstanceInput(filters))
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(InstanceOutput(query.Reservations))
+	fmt.Println(instanceOutput(query.Reservations))
 	if action == "list" {
 		os.Exit(0)
 	}
-	fmt.Printf("Are you sure you would like to %s the above instances (y|n)\n", action)
+	fmt.Printf("Are you sure you would like to %s the above instances (y/n)\n", action)
 	var verify string
 	_, err = fmt.Scan(&verify)
 	if err != nil {
@@ -111,21 +36,19 @@ func main() {
 	case "y":
 		fmt.Printf("Performing %s action on instances\n", action)
 		if action == "stop" {
-			stopInstances := new(ec2.StopInstancesInput)
-			stopInstances.SetInstanceIds(InstanceIds(Instances(query.Reservations)))
-			output, err := svc.StopInstances(stopInstances)
+			output, err := svc.StopInstances(newStopInstanceInput(query))
 			if err != nil {
 				fmt.Println(err)
 			}
-			fmt.Println(InstanceStateOutput(output.StoppingInstances))
+			fmt.Println(instanceStateOutput(output.StoppingInstances))
 		} else if action == "start" {
-			startInstances := new(ec2.StartInstancesInput)
-			startInstances.SetInstanceIds(InstanceIds(Instances(query.Reservations)))
-			output, err := svc.StartInstances(startInstances)
+			output, err := svc.StartInstances(newStartInstanceInput(query))
 			if err != nil {
 				fmt.Println(err)
 			}
-			fmt.Println(InstanceStateOutput(output.StartingInstances))
+			fmt.Println(instanceStateOutput(output.StartingInstances))
+		} else {
+			fmt.Printf("Action %s not defined", action)
 		}
 	case "n":
 		fmt.Println("Exiting and taking no action")
